568f67129f3efeecaa36c2d2d25d89056c4e4bb8
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-04-19 23:02:07 +1200

    [Refactor] Use es6 for profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,66 +1,70 @@
-var React = require( 'react' );
-var Router = require( 'react-router' );
+import React from 'react';
 import UserProfile from './Github/UserProfile';
 import Repos from './Github/Repos';
 import Notes from './Notes/Notes';
-var ReactFireMixin = require( 'reactfire' );
-var Firebase = require( 'firebase' );
 import getGithubInfo from '../utils/helpers'
+import Rebase from 're-base';
 
-var Profile = React.createClass({
-    mixins: [ReactFireMixin],
-    getInitialState: function() {
-        return {
-            notes: [1, 2, 3],
-            bio: {},
-            repos: []
-        };
-    },
-    componentDidMount: function() {
-      this.ref = new Firebase( 'https://reacting-egghead.firebaseio.com/' );
-      this.init( this.props.params.username );
+const base = Rebase.createClass( 'https://reacting-egghead.firebaseio.com/' );
 
-    },
-    componentWillReceiveProps: function( nextProps ) {
-      this.unbind( 'notes' );
-      this.init( nextProps.params.username );
-    },
-    componentWillUnmount: function() {
-      this.unbind( 'notes' );
-    },
-    init: function( username ) {
-      var childRef = this.ref.child( username );
-      this.bindAsArray( childRef, 'notes' );
-
-      getGithubInfo( username )
-        .then( function( data ) {
-          this.setState({
-            bio: data.bio,
-            repos: data.repos
-          })
-        }.bind( this ));
-    },
-    handleAddNote: function( newNote ) {
-      this.ref.child( this.props.params.username ).child( this.state.notes.length ).set( newNote );
-    },
-    render: function() {
-        return (
-            <div className="row">
-                <div className="col-md-4">
-                    <UserProfile username={this.props.params.username} bio={this.state.bio} />
-                </div>
-                <div className="col-md-4">
-                    <Repos username={this.props.params.username} repos={this.state.repos} />
-                </div>
-                <div className="col-md-4">
-                    <Notes
-                      username={this.props.params.username}
-                      notes={this.state.notes}
-                      addNote={this.handleAddNote} />
-                </div>
-            </div>
-        );
+class Profile extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      notes: [],
+      bio: {},
+      repos: []
     }
-});
+  }
+  componentDidMount() {
+    this.init( this.props.params.username );
 
-module.exports = Profile;
+  }
+  componentWillReceiveProps( nextProps ) {
+    base.removeBinding( this.ref );
+    this.init( nextProps.params.username );
+  }
+  componentWillUnmount() {
+    base.removeBinding( this.ref );
+  }
+  init( username ) {
+    this.ref = base.bindToState( username, {
+      context: this,
+      asArray: true,
+      state: 'notes'
+    });
+
+    getGithubInfo( username )
+      .then( function( data ) {
+        this.setState({
+          bio: data.bio,
+          repos: data.repos
+        })
+      }.bind( this ));
+  }
+  handleAddNote( newNote ) {
+    base.post( this.props.params.username, {
+      data: this.state.notes.concat([ newNote ])
+    });
+  }
+  render() {
+      return (
+          <div className="row">
+              <div className="col-md-4">
+                  <UserProfile username={this.props.params.username} bio={this.state.bio} />
+              </div>
+              <div className="col-md-4">
+                  <Repos username={this.props.params.username} repos={this.state.repos} />
+              </div>
+              <div className="col-md-4">
+                  <Notes
+                    username={this.props.params.username}
+                    notes={this.state.notes}
+                    addNote={( newNote ) => this.handleAddNote( newNote )} />
+              </div>
+          </div>
+      );
+  }
+};
+
+export default Profile;
